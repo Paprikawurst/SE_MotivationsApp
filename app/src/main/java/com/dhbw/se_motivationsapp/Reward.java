24d3817ce0977c0f5e1e28e518ac8108cd1f4abd
Nico W <nico.wolffld@gmx.de>
2021-05-23 14:59:39 +0200

    coding action für reward buttons part 1

--- app/src/main/java/com/dhbw/se_motivationsapp/Reward.java
+++ app/src/main/java/com/dhbw/se_motivationsapp/Reward.java
@@ -1,5 +1,7 @@
 package com.dhbw.se_motivationsapp;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,41 +9,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Reward#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Reward extends Fragment {
+public class Reward extends Fragment implements View.OnClickListener
+{
+    View view;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Reward() {
+    public Reward()
+    {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Reward.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Reward newInstance(String param1, String param2) {
+    //TODO: was ist das
+    public static Reward newInstance() {
         Reward fragment = new Reward();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,30 +31,43 @@ public class Reward extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //TODO: adi fragen wegen nullpointerexception
+        SharedPreferences spref = getContext().getSharedPreferences("SP", 0);
+        int points = spref.getInt("points", 0);
 
+    }
 
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            //TODO: ieine logik hier hin
+            case R.id.button_Reward1:
+                //logic
+                break;
+            case R.id.button_Reward2:
+                //logic
+                break;
+            default:
+                break;
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        Button btnReward1 = view.findViewById(R.id.button_Reward1);
+        Button btnReward2 = view.findViewById(R.id.button_Reward2);
+
+        //Set EventListener for Buttons
+        btnReward1.setOnClickListener(this);
+        btnReward2.setOnClickListener(this);
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_reward, container, false);
     }
 
-
-    public void onButtonReward1Click (View view)
-    {
-
-    }
-
-    public void onButtonReward2Click (View view)
-    {
-
-    }
 
 }
