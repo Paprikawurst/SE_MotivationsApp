5bb5a368c13e2d408724a69fbeda53ea5c5ecdae
Nico W <nico.wolffld@gmx.de>
2021-05-25 17:27:03 +0200

    progress bar disappears when reward redeemed

--- app/src/main/java/com/dhbw/se_motivationsapp/Reward.java
+++ app/src/main/java/com/dhbw/se_motivationsapp/Reward.java
@@ -10,6 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,6 +21,9 @@ public class Reward extends Fragment implements View.OnClickListener
     private int points;
     private SharedPreferences spref;
     TextView points_text;
+    ProgressBar progressBarReward1;
+    ProgressBar progressBarReward2;
+
 
     public Reward()
     {
@@ -48,9 +53,13 @@ public class Reward extends Fragment implements View.OnClickListener
         Button btnReward1 = view.findViewById(R.id.button_Reward_1);
         Button btnReward2 = view.findViewById(R.id.button_Reward_2);
 
+        progressBarReward1 = view.findViewById(R.id.progressBar_Reward1);
+        progressBarReward2 = view.findViewById(R.id.progressBar_Reward2);
+
         //Set EventListener for Buttons
         btnReward1.setOnClickListener(this);
         btnReward2.setOnClickListener(this);
+        filterRedeemed();
 
         return view;
     }
@@ -62,10 +71,10 @@ public class Reward extends Fragment implements View.OnClickListener
         switch (v.getId())
         {
             case R.id.button_Reward_1:
-                onRedeemReward(10);
+                onRedeemReward(10, "Reward1");
                 break;
             case R.id.button_Reward_2:
-                onRedeemReward(20);
+                onRedeemReward(20, "Reward2");
                 break;
             default:
                 break;
@@ -73,17 +82,44 @@ public class Reward extends Fragment implements View.OnClickListener
     }
 
 
-    public void onRedeemReward(int rewardPoints)
+    public void onRedeemReward(int rewardPoints, String rewardName)
     {
         SharedPreferences.Editor editor = spref.edit();
         points = points + rewardPoints;
         editor.putInt("points", points);
         editor.commit();
         Toast.makeText(getContext() , "You just got " + rewardPoints + " points!", Toast.LENGTH_SHORT).show();
-
+        editor.putBoolean(rewardName ,true);
 
         View view_toolbar = getActivity().findViewById(R.id.toolbar);
         points_text = view_toolbar.findViewById(R.id.points);
         points_text.setText(String.valueOf(points));
+
+        if(rewardName.equals("Reward1"))
+        {
+            progressBarReward1.setVisibility(View.GONE);
+        }
+        else if(rewardName.equals("Reward2"))
+        {
+            progressBarReward2.setVisibility(View.GONE);
+        }
     }
+
+    public void filterRedeemed()
+    {
+
+        progressBarReward1.setEnabled(spref.getBoolean("purchased_red",false));
+
+        if(spref.getBoolean("Reward1", false))
+        {
+            progressBarReward1.setVisibility(View.GONE);
+        }
+        else if(spref.getBoolean("Reward2", false))
+        {
+            progressBarReward2.setVisibility(View.GONE);
+        }
+
+
+    }
+
 }
