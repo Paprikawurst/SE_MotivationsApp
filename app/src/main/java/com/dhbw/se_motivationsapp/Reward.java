eef3a7311f5fc34bd66b0e63a813c2d4503273db
Nico W <nico.wolffld@gmx.de>
2021-06-01 16:01:40 +0200

    rewards work

--- app/src/main/java/com/dhbw/se_motivationsapp/Reward.java
+++ app/src/main/java/com/dhbw/se_motivationsapp/Reward.java
@@ -12,7 +12,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -32,12 +31,6 @@ public class Reward extends Fragment implements View.OnClickListener
     Button btnReward4;
     Button btnReward5;
     Button btnReward6;
-    ProgressBar progressBarReward1;
-    ProgressBar progressBarReward2;
-    ProgressBar progressBarReward3;
-    ProgressBar progressBarReward4;
-    ProgressBar progressBarReward5;
-    ProgressBar progressBarReward6;
     ImageView checkReward1;
     ImageView checkReward2;
     ImageView checkReward3;
@@ -81,13 +74,6 @@ public class Reward extends Fragment implements View.OnClickListener
         btnReward4 = view.findViewById(R.id.button_Reward_4);
         btnReward5 = view.findViewById(R.id.button_Reward_5);
         btnReward6 = view.findViewById(R.id.button_Reward_6);
-
-        progressBarReward1 = view.findViewById(R.id.progressBar_Reward1);
-        progressBarReward2 = view.findViewById(R.id.progressBar_Reward2);
-        progressBarReward3 = view.findViewById(R.id.progressBar_Reward3);
-        progressBarReward4 = view.findViewById(R.id.progressBar_Reward4);
-        progressBarReward5 = view.findViewById(R.id.progressBar_Reward5);
-        progressBarReward6 = view.findViewById(R.id.progressBar_Reward6);
 
         //button color as set in shop, default dkgray
         btnReward1.setBackgroundColor(sPref.getInt("color", Color.DKGRAY));
@@ -158,41 +144,82 @@ public class Reward extends Fragment implements View.OnClickListener
     {
         SharedPreferences.Editor editor = sPref.edit();
         boolean canBeRedeemed = false;
+        String toastText = "blubb";
+        int totalGoals;
 
         //Reward1 Logic
         if(rewardName.equals("Reward1"))
         {
-            //TODO: Logik für reward 1
+            canBeRedeemed = true;
         }
 
         //Reward2 Logic
         else if(rewardName.equals("Reward2"))
         {
-            //TODO: Logik für reward 2
+            totalGoals = sPref.getInt("easyGoals", 0) + sPref.getInt("mediumGoals", 0) + sPref.getInt("hardGoals", 0);
+            if (totalGoals >= 1)
+            {
+                canBeRedeemed = true;
+            }
+            else {
+                Toast.makeText(getContext(), "You need to complete one goal to redeem this reward!", Toast.LENGTH_SHORT).show();
+            }
         }
 
         //Reward3 Logic
         else if(rewardName.equals("Reward3"))
         {
-            //TODO: Logik für reward 3
+            totalGoals = sPref.getInt("easyGoals", 0);
+            if(totalGoals >= 5)
+            {
+                canBeRedeemed = true;
+
+            }
+            else {
+                Toast.makeText(getContext(), "You need to complete " + (5 - totalGoals) +  " more easy goals to redeem this reward!", Toast.LENGTH_SHORT).show();
+            }
         }
 
         //Reward4 Logic
         else if(rewardName.equals("Reward4"))
         {
-            //TODO: Logik für reward 4
+            totalGoals = sPref.getInt("mediumGoals", 0);
+            if(totalGoals >= 3)
+            {
+                canBeRedeemed = true;
+
+            }
+            else {
+                Toast.makeText(getContext(), "You need to complete " + (3 - totalGoals) +  " more medium goals to redeem this reward!", Toast.LENGTH_SHORT).show();
+            }
         }
 
         //Reward5 Logic
         else if(rewardName.equals("Reward5"))
         {
-            //TODO: Logik für reward 5
+            totalGoals = sPref.getInt("hardGoals", 0);
+            if(totalGoals >= 1)
+            {
+                canBeRedeemed = true;
+
+            }
+            else {
+                Toast.makeText(getContext(), "You need to complete one hard goal to redeem this reward!", Toast.LENGTH_SHORT).show();
+            }
         }
 
         //Reward6 Logic
         else if(rewardName.equals("Reward6"))
         {
-            //TODO: Logik für reward 6
+             totalGoals = sPref.getInt("easyGoals", 0) + sPref.getInt("mediumGoals", 0) + sPref.getInt("hardGoals", 0);
+            if (totalGoals >= 10)
+            {
+                canBeRedeemed = true;
+            }
+            else {
+                    Toast.makeText(getContext(), "You need to complete " + (10 - totalGoals) + " more goals to redeem this reward!", Toast.LENGTH_SHORT).show();
+            }
+
         }
 
 
@@ -211,52 +238,46 @@ public class Reward extends Fragment implements View.OnClickListener
         }
         else if (!canBeRedeemed)
         {
-            Toast.makeText(getContext(), "You can not redeem this reward yet!", Toast.LENGTH_SHORT).show();
+
         }
     }
 
 
-    //check which reward has been redeemed and show progressbar, button and check accordingly
+    //check which reward has been redeemed and show button and check accordingly
     public void filterRedeemed() {
         if (sPref.getBoolean("Reward1redeemed", false))
         {
             btnReward1.setVisibility(View.GONE);
-            progressBarReward1.setVisibility(View.GONE);
             checkReward1.setVisibility(View.VISIBLE);
         }
 
         if (sPref.getBoolean("Reward2redeemed", false))
         {
             btnReward2.setVisibility(View.GONE);
-            progressBarReward2.setVisibility(View.GONE);
             checkReward2.setVisibility(View.VISIBLE);
         }
 
         if (sPref.getBoolean("Reward3redeemed", false))
         {
             btnReward3.setVisibility(View.GONE);
-            progressBarReward3.setVisibility(View.GONE);
             checkReward3.setVisibility(View.VISIBLE);
         }
 
         if (sPref.getBoolean("Reward4redeemed", false))
         {
             btnReward4.setVisibility(View.GONE);
-            progressBarReward4.setVisibility(View.GONE);
             checkReward4.setVisibility(View.VISIBLE);
         }
 
         if (sPref.getBoolean("Reward5redeemed", false))
         {
             btnReward5.setVisibility(View.GONE);
-            progressBarReward5.setVisibility(View.GONE);
             checkReward5.setVisibility(View.VISIBLE);
         }
 
         if (sPref.getBoolean("Reward6redeemed", false))
         {
             btnReward6.setVisibility(View.GONE);
-            progressBarReward6.setVisibility(View.GONE);
             checkReward6.setVisibility(View.VISIBLE);
         }
     }
