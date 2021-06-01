d8082608606f9a735c7d1c6eb8077303d9188c90
luchrist <luca.christ@gmx.de>
2021-06-01 10:20:27 +0200

    check past date and check title.lenght > 0 @addGoal

--- app/src/main/java/com/dhbw/se_motivationsapp/AddGoal.java
+++ app/src/main/java/com/dhbw/se_motivationsapp/AddGoal.java
@@ -4,8 +4,11 @@ package com.dhbw.se_motivationsapp;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,6 +29,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Calendar;
 
@@ -158,21 +162,21 @@ public class AddGoal extends Fragment implements View.OnClickListener {
             ArrayList<String> subgoals = new ArrayList<>();
 
             String t = String.valueOf(title.getText());
-            String end_date = (String) dateButton.getText();
+            if (t.length() > 0) {
+                String end_date = (String) dateButton.getText();
+                if (isFuture(end_date)) {
+                    Goal newGoal = new Goal(t, String.valueOf(description.getText()), end_date, notification.isChecked(),
+                            dif, subgoals);
+
+                    String goalstr = objectToJson(newGoal);
+
+                    SharedPreferences.Editor editor = sp.edit();
+                    int gnum = sp.getInt("goalnumber", 0);
+                    gnum++;
+                    editor.putInt("goalnumber", gnum);
 
 
-            Goal newGoal = new Goal(t, String.valueOf(description.getText()), end_date, notification.isChecked(),
-                    dif, subgoals);
-
-            String goalstr = objectToJson(newGoal);
-
-            SharedPreferences.Editor editor = sp.edit();
-            int gnum = sp.getInt("goalnumber", 0);
-            gnum++;
-            editor.putInt("goalnumber", gnum);
-
-
-            String key = "goal" + String.valueOf(gnum);
+                    String key = "goal" + String.valueOf(gnum);
             /*Set<String> goalset = new HashSet<>();
             goalset.add(String.valueOf(title.getText()));
             goalset.add(newGoal.getDescription());
@@ -194,21 +198,48 @@ public class AddGoal extends Fragment implements View.OnClickListener {
 
              */
 
-            editor.putString(key, goalstr);
-            editor.commit();
-            Toast.makeText(getContext(), "Sucessfully added", Toast.LENGTH_SHORT).show();
-            dateButton.setText(getTodaysDate());
-            if (easy.isChecked()) {
-                easy.setChecked(false);
-            } else if (medium.isChecked()) {
-                medium.setChecked(false);
-            } else if (hard.isChecked()) {
-                hard.setChecked(false);
-            }
-            title.setText("exampleGoal");
-            description.setText("");
-            if (notification.isChecked()) {
-                notification.setChecked(false);
+                    editor.putString(key, goalstr);
+                    editor.commit();
+                    Toast.makeText(getContext(), "Sucessfully added", Toast.LENGTH_SHORT).show();
+                    dateButton.setText(getTodaysDate());
+                    if (easy.isChecked()) {
+                        easy.setChecked(false);
+                    } else if (medium.isChecked()) {
+                        medium.setChecked(false);
+                    } else if (hard.isChecked()) {
+                        hard.setChecked(false);
+                    }
+                    title.setText("exampleGoal");
+                    description.setText("");
+                    if (notification.isChecked()) {
+                        notification.setChecked(false);
+                    }
+
+                } else {
+                    new androidx.appcompat.app.AlertDialog.Builder(requireContext())
+                            .setTitle("Past Date!")
+                            .setMessage("Deadline date is in the past.")
+                            .setCancelable(true)
+                            .setPositiveButton("Change", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+
+                            }).show();
+                }
+            } else {
+                new androidx.appcompat.app.AlertDialog.Builder(requireContext())
+                        .setTitle("Missing title!")
+                        .setMessage("title field can't be empty")
+                        .setCancelable(true)
+                        .setPositiveButton("Change", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+
+                        }).show();
             }
 
 
@@ -216,6 +247,87 @@ public class AddGoal extends Fragment implements View.OnClickListener {
             datePickerDialog.show();
 
         }
+    }
+
+    private boolean isFuture(String enddate) {
+
+        //  System.out.println("Enddatum" + enddate);
+        String startdate;
+        LocalDate today = LocalDate.now();
+        startdate = String.valueOf(today);
+
+        int month_end = getMonthInt(enddate);
+        char[] start = new char[startdate.length()];
+        char[] end = new char[enddate.length()];
+        //String convert to chararray
+        for (int i = 0; i < startdate.length(); i++) {
+            start[i] = startdate.charAt(i);
+        }
+        for (int i = 0; i < enddate.length(); i++) {
+            end[i] = enddate.charAt(i);
+        }
+        //new Strings of chars (parts of the date)
+        String year = String.copyValueOf(start, 0, 4);
+        int y = Integer.parseInt(year);
+
+        String month = String.copyValueOf(start, 5, 2);
+        int m = Integer.parseInt(month);
+
+        String day = String.copyValueOf(start, 8, 2);
+        int d = Integer.parseInt(day);
+
+        String endyear = String.copyValueOf(end, end.length - 4, 4);
+        int endy = Integer.parseInt(endyear);
+        String endday;
+        int endd;
+        if (Character.isDigit(end[1])) {
+            endday = String.copyValueOf(end, 0, 2);
+            endd = Integer.parseInt(endday);
+        } else {
+            endday = String.copyValueOf(end, 0, 1);
+            endd = Integer.parseInt(endday);
+        }
+
+
+        if (y < endy || month_end > m && y == endy || endd >= d && month_end == m && y == endy) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private int getMonthInt(String month) {
+        int i;
+        if (month.contains("Jan")) {
+            i = 1;
+        } else if (month.contains("Feb")) {
+            i = 2;
+        } else if (month.contains("Mar")) {
+            i = 3;
+        } else if (month.contains("Apr")) {
+            i = 4;
+        } else if (month.contains("May")) {
+            i = 5;
+        } else if (month.contains("Jun")) {
+            i = 6;
+        } else if (month.contains("Jul")) {
+            i = 7;
+        } else if (month.contains("Aug")) {
+            i = 8;
+        } else if (month.contains("Sep")) {
+            i = 9;
+        } else if (month.contains("Oct")) {
+            i = 10;
+        } else if (month.contains("Nov")) {
+            i = 11;
+        } else if (month.contains("Dec")) {
+            i = 12;
+        } else {
+            return 100;
+        }
+
+
+        return i;
     }
 
     public String getMonthFormat(int month) {
