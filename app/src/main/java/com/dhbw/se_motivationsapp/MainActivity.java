5d33584d9dbbc091ff5a9ddadbe8025d1643122d
luchrist <luca.christ@gmx.de>
2021-06-01 11:05:54 +0200

    Default Actionbar.hide()

--- app/src/main/java/com/dhbw/se_motivationsapp/MainActivity.java
+++ app/src/main/java/com/dhbw/se_motivationsapp/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
         points_text = findViewById(R.id.points);
         title = findViewById(R.id.variabel_text);
